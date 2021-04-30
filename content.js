415d6e2d5ea168284b9c8015e5cd8917fb5d5e5c
ocs194069 <194069@ocsjoho.onmicrosoft.com>
2021-04-30 14:40:55 +0900

    popup

--- content.js
+++
@@ -1,5 +0,0 @@
-let button = document.getElementById("creatememo");
-
-button.onclick = function(){
-    console.log("aa");
-};
---
+++ note.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let button = document.getElementById("creatememo");
+    button.onclick = function () {
+        alert();
+    }
+};
+
